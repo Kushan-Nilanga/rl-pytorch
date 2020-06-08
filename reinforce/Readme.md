2217fd0f55a04273051da443224b003a10ed6a71
Kushan-Nilanga <dknathalage@gmail.com>
2020-06-08 12:16:24 +1000

    rewrite code and readme

---
+++ reinforce/Readme.md
@@ -0,0 +1,7 @@
+# REINFORCE Algorithm (Monte-Carlo policy gradient)
+
+Monte-Carlo policy gradients proposed in [Policy Gradient Methods for
+Reinforcement Learning with Function
+Approximation,](https://papers.nips.cc/paper/1713-policy-gradient-methods-for-reinforcement-learning-with-function-approximation.pdf) uses several sample rollouts to collect and update policy parameters over time to obtain the optimal policy to maximize expected rewards.
+
+![Pseudo Code for Reinforce algorithm](https://i.stack.imgur.com/D0K5F.png)
--- reinforce/reinforce.py
+++ reinforce/reinforce.py
@@ -1,25 +1,40 @@
 import torch.nn as nn
+import torch.nn.functional as f
+from torch.distributions import Categorical
 import torch as t
 
 import gym
 
 
 class Agent(nn.Module):
-    def __init__(self, gamma=0.99, input_dims=4, l1_dims=256):
+    def __init__(self, gamma=0.99, input_dims=4, l1_dims=256, output_dims=2):
         super().__init__()  # nn.Module is initialised
         self.gamma = gamma
         self.device = "cuda:0" if t.cuda.is_available() else "cpu:1"
         self.l1 = nn.Linear(input_dims, l1_dims)
+        self.l2 = nn.Linear(l1_dims, output_dims)
+        self.optimizer = t.optim.Adam(self.parameters(), lr=1e-2)
 
     def print(self):
         print("device -", self.device)
         print("gamma -", self.gamma)
 
     def select_action(self, observation):
-        observation = t.tensor(observation).to(self.device)
-        return 1
+        action_probs = self.forward(observation)
+        dist = Categorical(action_probs)
+        action = dist.sample()
+        return action
 
-    def forward(self):
+    def forward(self, observation):
+        observation = t.tensor(observation).to(self.device)
+        x = f.relu(self.l1(observation))
+        x = f.softmax(self.l2(x), dim=-1)
+        return x
+
+    def learn(self):
+        self.zero_grad()
+        loss.backward()
+        self.optimizer.step()
 
 
 if __name__ == "__main__":
@@ -27,6 +42,10 @@ if __name__ == "__main__":
     agent.print()
     env = gym.make("CartPole-v0")
     observation = env.reset()
+
+    rews = []
+    dones = []
+    states = []
 
     episode_reward = 0
     for i in range(20):
@@ -36,6 +55,9 @@ if __name__ == "__main__":
             # action = env.action_space.sample()  # agent.select_action
             action = agent.select_action(observation)
             observation, reward, done, info = env.step(action)
+            rews.append(reward)
+            dones.append(done)
+            states.append(observation)
             episode_reward += reward
             if done:
                 observation = env.reset()
@@ -44,4 +66,8 @@ if __name__ == "__main__":
                 break
         if (i % 10 == 0):
             print("learning")
+            agent.learn(rews, dones, states)
+            rews.clear()
+            dones.clear()
+            states.clear()
     env.close()
