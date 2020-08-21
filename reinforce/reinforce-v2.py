22b8688e2d4cc111126f8ada051a8b6ad5fe4a12
Kushan-Nilanga <dknathalage@gmail.com>
2020-08-21 17:24:26 +1000

    minor changes

---
+++ reinforce/reinforce-v2.py
@@ -0,0 +1,9 @@
+import torch as t
+import torch.nn as nn
+import torch.nn.functional as function
+
+import gym
+
+if __name__ == '__main__':
+    print(f"using gym version {gym.__version__}")
+    print(f"using pytorch version {t.__version__}")
